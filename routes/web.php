8248d75760fc59b68ef5c3ad7c76879e434c9c13
Ivan_estuvo_aqui <ivaneliecer2004@gmail.com>
2025-02-27 12:22:31 -0500

    welcome del ecomenrce

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,34 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('Products', function () {
+    echo "listado de todos los productos";
+});
+
+Route::get('Products/create', function () {
+    echo "formulario para crear un producto";
+});
+
+Route::get('Products/computados', function () {
+    echo "producto: computador";
+});
+Route::get('Products/teclado', function () {
+    echo "producto: computador";
+});
+Route::get('Products/mouse', function () {
+    echo "producto: mouse";
+});
+
+Route::get('Products/{name}', function ($name) {
+    echo "producto: $name";
+});
+
+Route::get('Products/{name}/{category}', function ($name, $category) {
+    echo "producto: $name, y tiene una categoria $category";
 });
