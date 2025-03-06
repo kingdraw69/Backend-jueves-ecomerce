d639264f3ac5eb97a127bdd325b7f50640847e16
Ivan_estuvo_aqui <ivaneliecer2004@gmail.com>
2025-03-06 12:04:55 -0500

    group controller

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,13 @@ use App\Http\Controllers\HomeControler;
 use App\Http\Controllers\ProductController;
 
 Route::get('/', HomeControler::class);
-Route::get('products',[ProductController::class, 'index'] );
-Route::get('products/create',[ProductController::class, 'create'] )->name('products.create');
-Route::get('products/{name}',[ProductController::class, 'show'] );
+
+Route::prefix('/products')->controller((ProductController::class))->group(function () {
+    Route::get('/', 'index' );
+    Route::get('/create','create' );
+    Route::get('/{name}','show');
+});
+
 
 //Route::get('Products/{name}/{category}', function ($name, $category) {
 //    echo "producto: $name, y tiene una categoria $category";
