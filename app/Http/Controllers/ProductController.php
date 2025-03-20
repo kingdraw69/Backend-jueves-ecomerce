6e1f0fabca80ca354006b6af5f9f95768c58d4d4
Ivan_estuvo_aqui <ivaneliecer2004@gmail.com>
2025-03-20 11:45:14 -0500

    llaves foraneas

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,13 +1,19 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function index(){
-        return view('products.index');//retornar un lista de productos
+
+        $productos = Product::all(); 
+        $productos = [
+            (object) ['id' => 1, 'nombre' => 'Producto 1', 'imagen' => 'https://via.placeholder.com/150', 'precio' => 50000],
+            (object) ['id' => 2, 'nombre' => 'Producto 2', 'imagen' => 'https://via.placeholder.com/150', 'precio' => 75000],
+        ];
+        return view('products.index', compact('productos'));//retornar un lista de productos
     }
     public function create(){
         return view('products.create'); // Cargar la vista de creación de productos
